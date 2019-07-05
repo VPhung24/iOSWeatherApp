ad897e17cf48cfcb5cda648f4469bc940348dfef
Vivian Phung <vivianphung@outlook.com>
2019-07-04 17:07:38 -0700

    first commit

---
+++ Project/FiveDayViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FiveDayViewController.swift
+//  Project
+//
+//  Created by Vivian Phung on 7/3/19.
+//  Copyright © 2019 Vivian Phung. All rights reserved.
+//
+
+import Foundation
---
+++ Project/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Project
+//
+//  Created by Vivian Phung on 6/27/19.
+//  Copyright © 2019 Vivian Phung. All rights reserved.
+//
+
+import Foundation
